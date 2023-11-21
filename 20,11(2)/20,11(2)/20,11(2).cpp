d79c57615dbc622caf3ab20ba1e86b8231548238
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-11-21 20:56:23 +0200

    Add files via upload

---
+++ 20,11(2)/20,11(2)/20,11(2).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+int countWordOccurrences(const std::string& text, const std::string& word) {
+    size_t pos = 0;
+    int count = 0;
+    std::string lowerText = text;
+    std::string lowerWord = word;
+
+    std::transform(lowerText.begin(), lowerText.end(), lowerText.begin(), ::tolower);
+    std::transform(lowerWord.begin(), lowerWord.end(), lowerWord.begin(), ::tolower);
+
+    while ((pos = lowerText.find(lowerWord, pos)) != std::string::npos) {
+        count++;
+        pos += lowerWord.length();
+    }
+
+    return count;
+}
+
+int main() {
+    std::string wordToFind;
+
+    std::cout << "Enter the word to find in the file (only lowercase letters): ";
+    std::cin >> wordToFind;
+
+    std::cout << "Enter the content of the file (terminate input with Ctrl+D in Unix-like systems or Ctrl+Z in Windows):\n";
+
+    std::string text;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        text += line + "\n";
+    }
+
+    int occurrences = countWordOccurrences(text, wordToFind);
+    std::cout << "The word '" << wordToFind << "' appears " << occurrences << " times in the entered text." << std::endl;
+
+    return 0;
+}
---
+++ 20,11/20,11/20,11.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+int main() {
+    FILE* file = std::fopen("text.txt", "r");
+    if (!file) {
+        std::cerr << "Unable to open file!" << std::endl;
+        return 1;
+    }
+
+    char buffer[BUFSIZ]; 
+    int maxLength = 0;
+
+    while (std::fgets(buffer, BUFSIZ, file)) {
+        int length = std::strlen(buffer);
+        if (buffer[length - 1] == '\n') {
+            length -= 1;
+        }
+        maxLength = (length > maxLength) ? length : maxLength;
+    }
+
+    std::fclose(file);
+
+    if (maxLength > 0) {
+        std::cout << "Length of the longest line in the file: " << maxLength << " characters." << std::endl;
+    }
+    else {
+        std::cout << "The file is empty." << std::endl;
+    }
+
+    return 0;
+}
